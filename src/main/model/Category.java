9cebfa07cf7407a7e654894fcf507e43717e42bb
Sytvn <steven2003huang@gmail.com>
2023-02-24 11:08:29 -0800

    Changed tests to fix ID value

--- src/main/model/Category.java
+++ src/main/model/Category.java
@@ -2,7 +2,7 @@ package model;
 
 public class Category {
     private String name;           // name of the category
-    private double value = 0;          // numerical value of a category
+    private double value;          // numerical value of a category
 
     // REQUIRES: non-zero categoryName length and categoryValue >=  0
     // EFFECT: name is set to categoryName and value is set to categoryValue
@@ -22,21 +22,21 @@ public class Category {
     // MODIFIES: this
     // EFFECT: categoryValue is set to given value and returned
     public double setValue(double setValue) {
-        value = setValue;
+        this.value = setValue;
         return value;
     }
 
     // MODIFIES: this
     // EFFECT: categoryValue decreases by addValue and returns value
     public double subValue(double subValue) {
-        value -= subValue;
+        this.value = value - subValue;
         return value;
     }
 
     // MODIFIES: this
     // EFFECT: categoryValue increases by addValue and returns value
     public double addValue(double addValue) {
-        value += addValue;
+        this.value = value + addValue;
         return value;
     }
 
